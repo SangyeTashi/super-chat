9101b456a9b43e4591a3f5f1726d00ed0a3316c2
sangyetashi <sangyetashi2@gmail.com>
2023-04-02 10:35:28 +0530

    smooth scroll

--- src/ChatBox.tsx
+++ src/ChatBox.tsx
@@ -3,20 +3,26 @@ import { useEffect, useRef } from 'react';
 import { useList } from 'react-firebase-hooks/database';
 import { database } from '../firebase';
 import Message from './Message';
-
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 const ChatBox = () => {
+    // Get messages
     const [messages, messageLoading, messageError] = useList(
         ref(database, 'messages/')
     );
     const scrollParentRef = useRef<HTMLDivElement>(null);
 
+    //Scroll new messages into view
     useEffect(() => {
-        scrollParentRef.current?.lastElementChild?.scrollIntoView();
+        scrollParentRef.current?.lastElementChild?.scrollIntoView({
+            behavior: 'smooth',
+        });
     }, [messages]);
     return (
         <div className=" p-4 shrink space-y-4 overflow-scroll">
             {messageError && <h1> Error : {messageError.message}</h1>}
             {messageLoading && <span>messages loading...</span>}
+
+            {/* map and render all messages */}
             {messages && (
                 <div
                     ref={scrollParentRef}
